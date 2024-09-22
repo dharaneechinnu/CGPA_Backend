e7c9b62c7520254d4e58bfb84803191435bf6a4c
Your Name <your.email@example.com>
2024-09-22 23:18:36 +0530

    backend

--- Server.js
+++ Server.js
@@ -1,27 +1,36 @@
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = process.env.PORT|| 3500;
+const PORT = process.env.PORT || 3500;
 const MONGODB_URL = process.env.MONGO_URL;
+
 const app = express();
-const User = require('./Model/User')
+
+// Connect to MongoDB
 mongoose.connect(MONGODB_URL)
   .then(() => {
-     console.log('Database is connected');
+    console.log('Database is connected');
   })
   .catch((err) => {
     console.error('Error connecting to the database:', err.message);
   });
-  
-app.use(cors())
-app.use(express.json())
 
-app.use('/Auth',require('./Router/Router'))
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/Auth', require('./Router/Router'));
 app.use('/api', require('./Router/Target'));
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
-app.listen(PORT,() =>{
-  
-    console.log(`Server is Running... ${PORT} `);
-  })
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
